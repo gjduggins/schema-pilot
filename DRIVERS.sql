5310fffd442a11eebe0ed26a0e071728b5ef4ee4
Garry Duggins <g_duggins@sky.com>
2025-12-10 12:29:08 +0000

    Add first SQL Files

---
+++ DRIVERS.sql
@@ -0,0 +1,5 @@
+create table DRIVERS (
+    ID number,
+    USERNAME varchar2(50),
+    EMAIL varchar2(100)
+);
---
+++ RACES.sql
@@ -0,0 +1,6 @@
+create table RACES (
+    ID number,
+    RACENAME varchar2(50),
+    COUNTRY varchar2(100),
+    DISTANCE number(5,2)
+);
